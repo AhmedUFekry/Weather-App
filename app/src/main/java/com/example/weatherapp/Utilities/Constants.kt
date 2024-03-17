44b7bee4164052fcb44cb3a388d32c0abf03c378
Ahmed Fekry <ahmedfekry792@gmail.com>
2024-03-17 07:50:25 +0200

    fix api key issue

--- app/src/main/java/com/example/weatherapp/Utilities/Constants.kt
+++ app/src/main/java/com/example/weatherapp/Utilities/Constants.kt
@@ -1,12 +1,6 @@
 package com.example.weatherapp.Utilities
 
 object Constants {
-    const val TAG = "TAG"
-    const val DatabaseName = ""
-    const val FavNavType = "Favorite"
     const val BASE_URL ="https://api.openweathermap.org/data/3.0/"
-    // API Data
-    const val API_KEY = "3e8a36a0cdc52b973b2703a33477b75e"
-    const val EXCLUDE="minutely"
-    const val UNITS = "metric"
+    const val API_KEY = "3302285cf5de6dc7c5eff562fe63a8c7"
 }
